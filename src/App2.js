de5f9fbe26f175178fe6ec2709ae7e68decd12ee
Alex Widner <manny1806@gmail.com>
2019-02-15 22:22:03 -0800

    progress

--- src/App2.js
+++ src/App2.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./App2.css";
 import "./moving-box.css"
+import "./right-box.css"
 class App2 extends Component {
     constructor() {
       super()
@@ -9,8 +10,11 @@ class App2 extends Component {
         innerLeftBoxClass: "inner-left-box-bio",
         leftMovingBoxClass: "left-moving-box-bio",
         rightBoxClass: "right-box-bio",
+        rightMovingBoxClass: "right-moving-box-bio",
         movingBoxContentBioClass: "moving-box-content-bio-active",
-        movingBoxContentSkillClass: "moving-box-content-skills-vert"
+        movingBoxContentSkillClass: "moving-box-content-skills-vert",
+        movingBoxContentProjectsClass: "moving-box-content-projects-horz",
+        movingBoxContentConnectClass: "moving-box-content-connect-horz"
       }
       
     }
@@ -28,13 +32,21 @@ class App2 extends Component {
                                         backgroundClass: "background-skills",
                                         innerLeftBoxClass: "inner-left-box-skills",
                                         leftMovingBoxClass: "left-moving-box-skills",
-                                        rightBoxClass: "right-box-skills"
+                                        rightBoxClass: "right-box-skills",
+                                        movingBoxContentBioClass: "moving-box-content-bio-vert",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-active",
+                                        movingBoxContentProjectsClass: "moving-box-content-projects-horz",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-vert"
                                     })
                                     setTimeout(()=>{this.setState({
                                         backgroundClass: "background-bio",
                                         innerLeftBoxClass: "inner-left-box-bio",
                                         leftMovingBoxClass: "left-moving-box-bio",
-                                        rightBoxClass: "right-box-bio"
+                                        rightBoxClass: "right-box-bio",
+                                        movingBoxContentBioClass: "moving-box-content-bio-active",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-vert",
+                                        movingBoxContentProjectsClass: "moving-box-content-projects-horz",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-horz"
                                     })}, 1000)
                                 } else {
                                     this.setState({
@@ -42,24 +54,35 @@ class App2 extends Component {
                                         innerLeftBoxClass: "inner-left-box-bio",
                                         leftMovingBoxClass: "left-moving-box-bio",
                                         rightBoxClass: "right-box-bio",
+                                        rightMovingBoxClass: "right-moving-box-bio",
                                         movingBoxContentBioClass: "moving-box-content-bio-active",
-                                        movingBoxContentSkillClass: "moving-box-content-skills-vert"
+                                        movingBoxContentSkillClass: "moving-box-content-skills-vert",
+                                        movingBoxContentProjectsClass: "moving-box-content-projects-horz",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-horz"
                                     })
                                 }
                                 }}><span>bio</span></label>
-                                <label className = "connect-button" onClick={()=>{
+                            <label className = "connect-button" onClick={()=>{
                                 if(this.state.backgroundClass === "background-skills"){
                                     this.setState({
                                         backgroundClass: "background-projects",
                                         innerLeftBoxClass: "inner-left-box-projects",
                                         leftMovingBoxClass: "left-moving-box-projects",
-                                        rightBoxClass: "right-box-projects"
+                                        rightBoxClass: "right-box-projects",
+                                        movingBoxContentBioClass: "moving-box-content-bio-vert",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-horz",
+                                        movingBoxContentProjectsClass: "moving-box-content-skills-active",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-vert"
                                     })
                                     setTimeout(()=>{this.setState({
                                         backgroundClass: "background-connect",
                                         innerLeftBoxClass: "inner-left-box-connect",
                                         leftMovingBoxClass: "left-moving-box-connect",
-                                        rightBoxClass: "right-box-connect"
+                                        rightBoxClass: "right-box-connect",
+                                        movingBoxContentBioClass: "moving-box-content-bio-horz",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-horz",
+                                        movingBoxContentProjectsClass: "moving-box-content-skills-vert",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-active"
                                     })}, 1000)
                                 } else {
                                     this.setState({
@@ -67,7 +90,11 @@ class App2 extends Component {
                                         innerLeftBoxClass: "inner-left-box-connect",
                                         leftMovingBoxClass: "left-moving-box-connect",
                                         rightBoxClass: "right-box-connect",
-                                        movingBoxContentBioClass: "moving-box-content-bio-horz"
+                                        rightMovingBoxClass: "right-moving-box-connect",
+                                        movingBoxContentBioClass: "moving-box-content-bio-horz",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-horz",
+                                        movingBoxContentProjectsClass: "moving-box-content-skills-vert",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-active"
                                     })
                                 }
                                 }}>connect</label>
@@ -77,13 +104,21 @@ class App2 extends Component {
                                         backgroundClass: "background-projects",
                                         innerLeftBoxClass: "inner-left-box-projects",
                                         leftMovingBoxClass: "left-moving-box-projects",
-                                        rightBoxClass: "right-box-projects"
+                                        rightBoxClass: "right-box-projects",
+                                        movingBoxContentBioClass: "moving-box-content-bio-vert",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-horz",
+                                        movingBoxContentProjectsClass: "moving-box-content-skills-active",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-vert"
                                     })
                                     setTimeout(()=>{this.setState({
                                         backgroundClass: "background-skills",
                                         innerLeftBoxClass: "inner-left-box-skills",
                                         leftMovingBoxClass: "left-moving-box-skills",
-                                        rightBoxClass: "right-box-skills"
+                                        rightBoxClass: "right-box-skills",
+                                        movingBoxContentBioClass: "moving-box-content-bio-vert",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-active",
+                                        movingBoxContentProjectsClass: "moving-box-content-projects-horz",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-vert"
                                     })}, 1000)
                                 } else {
                                     this.setState({
@@ -91,8 +126,11 @@ class App2 extends Component {
                                         innerLeftBoxClass: "inner-left-box-skills",
                                         leftMovingBoxClass: "left-moving-box-skills",
                                         rightBoxClass: "right-box-skills",
+                                        rightMovingBoxClass: "right-moving-box-skills",
                                         movingBoxContentBioClass: "moving-box-content-bio-vert",
-                                        movingBoxContentSkillClass: "moving-box-content-skills-active"
+                                        movingBoxContentSkillClass: "moving-box-content-skills-active",
+                                        movingBoxContentProjectsClass: "moving-box-content-projects-horz",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-vert"
                                     })
                                 }
                                 }}>skills</label>
@@ -102,20 +140,33 @@ class App2 extends Component {
                                         backgroundClass: "background-skills",
                                         innerLeftBoxClass: "inner-left-box-skills",
                                         leftMovingBoxClass: "left-moving-box-skills",
-                                        rightBoxClass: "right-box-skills"
+                                        rightBoxClass: "right-box-skills",
+                                        movingBoxContentBioClass: "moving-box-content-bio-vert",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-active",
+                                        movingBoxContentProjectsClass: "moving-box-content-projects-horz",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-vert"
                                     })
                                     setTimeout(()=>{this.setState({
                                         backgroundClass: "background-projects",
                                         innerLeftBoxClass: "inner-left-box-projects",
                                         leftMovingBoxClass: "left-moving-box-projects",
-                                        rightBoxClass: "right-box-projects"
+                                        rightBoxClass: "right-box-projects",
+                                        movingBoxContentBioClass: "moving-box-content-bio-vert",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-horz",
+                                        movingBoxContentProjectsClass: "moving-box-content-skills-active",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-vert"
                                     })}, 1000)
                                 } else {
                                     this.setState({
                                         backgroundClass: "background-projects",
                                         innerLeftBoxClass: "inner-left-box-projects",
                                         leftMovingBoxClass: "left-moving-box-projects",
-                                        rightBoxClass: "right-box-projects"
+                                        rightBoxClass: "right-box-projects",
+                                        rightMovingBoxClass: "right-moving-box-projects",
+                                        movingBoxContentBioClass: "moving-box-content-bio-vert",
+                                        movingBoxContentSkillClass: "moving-box-content-skills-horz",
+                                        movingBoxContentProjectsClass: "moving-box-content-skills-active",
+                                        movingBoxContentConnectClass: "moving-box-content-connect-vert"
                                     })
                                 }
                                 }}>projects</label>
@@ -125,9 +176,23 @@ class App2 extends Component {
                     <div className = {`left-moving-box ${this.state.leftMovingBoxClass}`}>
                                 <div className = {`moving-box-content-bio ${this.state.movingBoxContentBioClass}`}>Alex Widner</div>
                                 <div className = {`moving-box-content-skills ${this.state.movingBoxContentSkillClass}`}>Full Stack</div>
+                                <div className = {`moving-box-content-projects ${this.state.movingBoxContentProjectsClass}`}>Projects</div>
+                                <div className = {`moving-box-content-connect ${this.state.movingBoxContentConnectClass}`}>Connect</div>
                     </div>
                 </div>
-                <div className = {`right-box ${this.state.rightBoxClass}`}></div>
+                <div className = {`right-box ${this.state.rightBoxClass}`}>
+                    <div className = {`moving-right-box-container ${this.state.rightMovingBoxClass}`}>
+                            <div className="right-bio-box">
+                                <p>I am a web developer based in Portland, OR.</p>
+                                <img src="https://res.cloudinary.com/siouxcitymusic/image/upload/v1550296888/portland.png"/>
+                                <p>However, I like to think of myself as a fanatic problem solver who
+                                    isn't afraid to think outside of the box.</p>
+                            </div>
+                            <div className="right-skills-box"></div>
+                            <div className="right-projects-box"></div>
+                            <div className="right-connect-box"></div>
+                    </div>
+                </div>
             </div>
         </div>
         )
